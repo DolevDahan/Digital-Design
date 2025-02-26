8fc48b6f7667e138c92a25825c9b989a21fcd8da
Dolev Dahan <134111596+Dolevdah@users.noreply.github.com>
2025-02-26 16:12:07 +0200

    Update uart_top.v

--- UART_PROTOCOL/uart_top.v
+++ UART_PROTOCOL/uart_top.v
@@ -6,13 +6,12 @@ module uart_top(
     input wire rx_data,
     output wire [7:0] data_out,
     output wire rx_done,
-    output wire [1:0] flag_state  // נוסיף חיבור ל-flag
+    output wire [1:0] flag_state
 );
 
     wire uart_tick;
     wire uart_half_tick;
 
-    // מחולל קצב השידור (Baud Rate Generator)
     baud_rate_generator baud_gen (
         .system_clk(system_clk),
         .rst(rst),
@@ -20,7 +19,6 @@ module uart_top(
         .uart_half_tick(uart_half_tick)
     );
 
-    // מקלט UART
     uart_receiver receiver (
         .system_clk(system_clk),
         .rst(rst),
@@ -29,7 +27,7 @@ module uart_top(
         .rx_data(rx_data),
         .data_out(data_out),
         .rx_done(rx_done),
-        .flag_state(flag_state)  // מחבר את ה-flag החוצה
+        .flag_state(flag_state) 
     );
 
 endmodule
